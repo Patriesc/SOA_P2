c594d7a8213ce95daa53f210f3b6fca23a55803b
Patriesc <98825807+Patriesc@users.noreply.github.com>
2022-11-17 12:25:18 +0100

    Add files via upload

--- procSecuencial.c
+++ procSecuencial.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DIMFILA 300000
+#define NUMFILAS 20
+typedef struct fila{
+int vector[DIMFILA];
+long suma;
+} fila_t;
+typedef fila_t * pfila_t; //Puntero a una estructura fila
+fila_t matriz[NUMFILAS]; 
+int i, fila;
+
+int main(int argc, char* argv[]) {
+  
+struct fila filaSec;
+  for(fila=0;fila<NUMFILAS;fila++){
+    filaSec.suma = 0;
+    
+    
+  }
+
+}
